64ac3f6086f72a1bc62ab58b78338c4874cebfa8
Chris Martin <chris.martin@redapplecreative.co.uk>
2019-10-09 12:04:14 +0100

    fix eslint styling

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -15,7 +15,7 @@ function SEO({ title }) {
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"
-      ></link>
+      />
     </Helmet>
   );
 }
